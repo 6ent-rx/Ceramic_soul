7f0f364ca3b44824c8b8532711cd003f6827251b
6ent-rx <tomanovv@bk.ru>
2025-07-19 20:46:54 +0300

    footer

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,34 @@
+import Swiper from 'swiper';
+import { Navigation, Pagination } from 'swiper/modules';
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+
 import "../sass/style.scss"
+
+const swiper = new Swiper('.swiper', {
+    modules: [Navigation, Pagination],
+    slidesPerView: 1,
+    loop: true,
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    breakpoints: {
+        1200: {
+            // when window is >= 1200
+            slidesPerView: 3,
+            spaceBetween: 5
+        },
+        1920: {
+            // when window is >= 1920
+            slidesPerView: 3,
+            spaceBetween: 35
+        },
+    },
+});
+
