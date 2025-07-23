be9c2ed65fa388f44f3c16fba3f7ab66286520ec
6ent-rx <tomanovv@bk.ru>
2025-07-23 03:16:48 +0300

    menu

--- src/js/script.js
+++ src/js/script.js
@@ -6,6 +6,20 @@ import 'swiper/css/pagination';
 
 import "../sass/style.scss"
 
+const burger = document.getElementsByClassName('hdr__burger');
+const menu = document.getElementsByClassName('hdr__menu')
+const closed = document.getElementsByClassName('hdr__menu-closed');
+
+burger[0].addEventListener('click', function () {
+    menu[0].classList.add('hdr__menu_active');
+    document.body.style = 'overflow: hidden;'
+})
+
+closed[0].addEventListener('click', function () {
+    menu[0].classList.remove('hdr__menu_active')
+    document.body.style = 'overflow: inherit;'
+})
+
 const swiper = new Swiper('.swiper', {
     modules: [Navigation, Pagination],
     slidesPerView: 1,
@@ -31,4 +45,8 @@ const swiper = new Swiper('.swiper', {
         },
     },
 });
+
+
+
+
 
